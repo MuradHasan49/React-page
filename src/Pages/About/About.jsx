5e98f79a15aa8d710c673e0de5794c8433dd6dd6
Murad Hasan <muradhasanshuvo00@gmail.com>
2025-11-23 20:41:26 +0600

    add auto play

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -20,6 +20,10 @@ const About = () => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    
   };
 
   return (
